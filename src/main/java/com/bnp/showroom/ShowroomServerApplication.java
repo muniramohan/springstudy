15c82692552a80f5c8de74b9c60ead88ac01a6ee
muniramohan <muni.rammohan@gmail.com>
2018-08-17 00:27:09 +0200

    packaging configuration

--- src/main/java/com/bnp/showroom/ShowroomServerApplication.java
+++ src/main/java/com/bnp/showroom/ShowroomServerApplication.java
@@ -3,6 +3,7 @@ package com.bnp.showroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,7 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //@EnableJpaAuditing
 @EnableJpaRepositories("com.bnp.showroom")
 @SpringBootApplication
-public class ShowroomServerApplication {
+public class ShowroomServerApplication extends SpringBootServletInitializer {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
